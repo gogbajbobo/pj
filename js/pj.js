1f7517f8c090b86dd5aae871558c62b2f2fe0757
gogbajbobo <grigoriev.maxim@gmail.com>
2017-05-23 16:33:31 +0300

    scrollToSection()

--- js/pj.js
+++ js/pj.js
@@ -4,5 +4,30 @@
 
 $(document).ready(function() {
 
+    scrollToSection();
+
 });
+
+function scrollToSection() {
+// Прокрутка к выбранному разделу
+
+    $(".navbar a, footer a[href='#pj-page']").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            let hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 140
+            }, 900, function() {
+                window.location.hash = hash;
+            });
+
+        }
+
+    });
+
+}
 
